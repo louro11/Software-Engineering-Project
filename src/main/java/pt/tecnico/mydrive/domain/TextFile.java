7c8c3816a7003cd2dfd5fef8f34082bec6bed652
MargaridaCorreia <margarida.correia@ist.utl.pt>
2016-03-12 21:52:33 +0000

    Adicionei o readfile

--- src/main/java/pt/tecnico/mydrive/domain/TextFile.java
+++ src/main/java/pt/tecnico/mydrive/domain/TextFile.java
@@ -1,5 +1,12 @@
 package pt.tecnico.mydrive.domain;
 
+import java.util.Scanner;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TextFile extends TextFile_Base {
     
     public TextFile() {
@@ -24,6 +31,38 @@ public class TextFile extends TextFile_Base {
     public boolean isAppendable(){
          
         return true;}
-		
-    
+
+    //@Override
+    public void readfile(){
+
+    //get path
+	Scanner input = new Scanner(System.in);
+	String path = input.nextLine();
+	
+	//get file name
+	int index = path.lastIndexOf("/");
+	String file = path.substring(index + 1);
+	System.out.println(file);
+	//tenho de verificar permissões??
+
+	//read file
+
+	try{
+		FileReader content = new FileReader(file);
+		BufferedReader readcontent = new BufferedReader(content);
+
+		String line = readcontent.readLine(); //read the 1st line
+
+		while (line!=null){
+			System.out.printf ("%s\n", line);
+
+			line = readcontent.readLine(); //read until the last line
+		}
+
+		content.close();
+	} catch (IOException e)
+{}
+
 }
+
+    }
