12982de44223006ce97fbd2bab58ce339cf26b35
louro11 <goncalo_louro@hotmail.com>
2016-03-12 16:30:25 +0000

    progress on xml

--- src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
+++ src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
@@ -1,9 +1,40 @@
 package pt.tecnico.mydrive.domain;
 
+import pt.ist.fenixframework.FenixFramework;
+import java.io.File;
+
+import org.jdom2.Element;
+import org.jdom2.Document;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.tecnico.mydrive.domain.FileSystem;
+
+import pt.tecnico.mydrive.exceptions.UserNameAlreadyExistsException; //criacao do user lanca esta excepcao
+
 public class MyDrive extends MyDrive_Base {
     
+	// Set de filesystems
+	
     public MyDrive() {
         super();
+    }
+    
+    public static MyDrive getInstance() {
+        MyDrive mydrive = FenixFramework.getDomainRoot().getMydrive();
+        //if (mydrive != null)
+        	return mydrive;
+    }
+    
+    public Document xmlExport() {
+        Element element = new Element("mydrive");
+        Document doc = new Document(element);
+
+        for (FileSystem fs: getFileSystemSet()) //filesystem tem de ter um set de user
+            element.addContent(fs.xmlExport());
+
+        return doc;
     }
     
 }
