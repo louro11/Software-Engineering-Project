ed4651631d8a9a5211f9a52834fe7334e5d53241
MargaridaCorreia <margarida.correia@ist.utl.pt>
2016-03-13 18:57:21 +0000

    construtor do mydrive

--- src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
+++ src/main/java/pt/tecnico/mydrive/domain/MyDrive.java
@@ -1,9 +1,13 @@
 package pt.tecnico.mydrive.domain;
 
 public class MyDrive extends MyDrive_Base {
-    
-    public MyDrive() {
-        super();
+
+    public MyDrive(){
+    	FileSystem fs = new FileSystem();
+
+    	setCurrentuser(fs.getRoot());
+    	setCurrentdirectory(fs.getMaindir());
+    	setFilesystem(fs);
     }
     
 }
