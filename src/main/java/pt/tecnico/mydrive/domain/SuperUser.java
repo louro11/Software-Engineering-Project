28d02516f15d1459cd696eeb9e5b5dc87e11cfd1
henrique <henrique.nave.carlos@gmail.com>
2016-03-13 16:12:06 +0000

    construtoes de user e super user

--- src/main/java/pt/tecnico/mydrive/domain/SuperUser.java
+++ src/main/java/pt/tecnico/mydrive/domain/SuperUser.java
@@ -1,9 +1,16 @@
 package pt.tecnico.mydrive.domain;
 
 public class SuperUser extends SuperUser_Base {
-    
+
     public SuperUser() {
         super();
     }
-    
+
+    public SuperUser(String username, String password, String name, String mask){
+      set_username;
+      set_password(password);
+      set_name(name);
+      set_mask(mask);
+    }
+
 }
--- src/main/java/pt/tecnico/mydrive/domain/User.java
+++ src/main/java/pt/tecnico/mydrive/domain/User.java
@@ -8,18 +8,19 @@ public class User extends User_Base {
       super();
     }
 
-    public User(String name) throws InvalidUserNameException {
-        super();
-
-        if( name.isEmpty() || name == null){
+    public User(String username, String password, String name, String mask) throws InvalidUserNameException {
+        set_password(password);
+        set_name(name);
+        set_mask(mask);
+        if( username.isEmpty() || username == null){
           throw new InvalidUserNameException("username is empty");
         }
         else{
              char c;
-             for (int i = 0; i < name.length(); i++) {
-                    c = name.charAt(i);
+             for (int i = 0; i < username.length(); i++) {
+                    c = username.charAt(i);
                     if(Character.isLetterOrDigit(c)){
-                      set_name(name);
+                      set_username(username);
                     }
                     else{
                       throw new InvalidUserNameException("username contains wrong Character");
