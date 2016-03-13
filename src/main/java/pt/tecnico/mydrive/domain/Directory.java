5fbf3c40849a0f3499b2f604e625ba98185b8edc
claudiaamorim <ana-claudia1995@hotmail.com>
2016-03-13 15:31:27 +0000

    tem o construtor do diretorio e a leitura de conteudo de diretorias

--- src/main/java/pt/tecnico/mydrive/domain/Directory.java
+++ src/main/java/pt/tecnico/mydrive/domain/Directory.java
@@ -1,28 +1,37 @@
 package pt.tecnico.mydrive.domain;
 
 import java.util.*;
+import org.joda.time.DateTime;
+import java.lang.String;
 
 
 public class Directory extends Directory_Base {
 
 
-    //nao sei se o super tem efeito
+
     public Directory() {
-        super();
+        super();	
+    }
+
+    public Directory(String name, Integer fileid, DateTime timestamp, String permission, User owner, Directory parent )
+    {  	
+       set_name(name); set_permission(permission); set_fileid(fileid);set_timestamp(timestamp);setOwner(owner);
+	   setParent(parent);
+	   setSelf(this);
     }
 
    //retorna listagem de todos os ficheiros(ficheiros ou diretorios de um diretorio)
    //o diretorio tem uma estrutura de files do tipo set
-   /*public String PrintFiles(){
-	   String s= "";
-	     //existe o metodo getFiles() --> retorna o set do diretorio
-	      Set<Files> _files = this.getFiles();
-	       for(Files f : _files) {
-		         s=s + f.getName()+"\n";
+
+   public String PrintFiles(){
+	     String s= "";
+	  
+	       for(File f : getFiles()) {
+		         s=s + f.get_name()+"\n";
 	          }
-	     s=s.substring(0,s.lenght()-1);
-	      return s;
-   }*/
+         //s = s.substring(0,s.length()-1);
+	     return s;
+   }
 
 
     @Override
