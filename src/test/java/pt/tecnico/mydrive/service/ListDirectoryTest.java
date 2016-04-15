43f11728860ab9adbf557416736f6424b48e8635
claudiaamorim <ana-claudia1995@hotmail.com>
2016-04-15 17:28:28 +0100

    Badjoras

---
+++ src/test/java/pt/tecnico/mydrive/service/ListDirectoryTest.java
@@ -0,0 +1,37 @@
+package pt.tecnico.mydrive.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pt.tecnico.mydrive.exceptions.InvalidPathException;
+import pt.tecnico.mydrive.exceptions.FileNotFoundException;
+
+
+import pt.tecnico.mydrive.domain.MyDrive;
+import pt.tecnico.mydrive.domain.User;
+import pt.tecnico.mydrive.domain.File;
+import pt.tecnico.mydrive.domain.Directory;
+import pt.tecnico.mydrive.domain.Login;
+
+
+public class ListDirectoryTest extends AbstractServiceTest {
+
+	private MyDrive md;
+
+
+	@Before
+    public void setUp(){
+
+        md = MyDrive.getInstance();
+	} 
+     
+    @After
+    public void tearDown() {
+        md=null;
+	 }
+
+	 
+
+
+}
