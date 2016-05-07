944fbe777344d9be4e959c3c14bc18735ae41df8
MargaridaCorreia <margarida.correia@ist.utl.pt>
2016-05-07 11:16:48 +0100

    merge conflicts

--- src/test/java/pt/tecnico/mydrive/service/ReadFileTest.java
+++ src/test/java/pt/tecnico/mydrive/service/ReadFileTest.java
@@ -1,92 +1,92 @@
-// package pt.tecnico.mydrive.service;
+package pt.tecnico.mydrive.service;
 
-// import java.util.*;
-// import org.joda.time.DateTime;
-
-
-// import static org.junit.Assert.*;
-
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
-
-// import mockit.Expectations;
-// import mockit.Verifications;
-// import mockit.Mocked;
-// import mockit.integration.junit4.JMockit;
-
-// import pt.tecnico.mydrive.exceptions.InvalidPathException;
-// import pt.tecnico.mydrive.exceptions.FileNotFoundException;
+import java.util.*;
+import org.joda.time.DateTime;
 
 
-// import pt.tecnico.mydrive.domain.MyDrive;
-// import pt.tecnico.mydrive.domain.User;
-// import pt.tecnico.mydrive.domain.File;
-// import pt.tecnico.mydrive.domain.Directory;
-// import pt.tecnico.mydrive.domain.Login;
+import static org.junit.Assert.*;
 
-// @RunWith(JMockit.class)
-// public class ReadFileTest extends AbstractServiceTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-// 	private static final String userName = "claudia";
-// 	private static final String fileName = "file.txt";
-// 	Random rand = new Random();
-//     long randomtoken = rand.nextLong();
+import mockit.Expectations;
+import mockit.Verifications;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
 
-// 	@Mocked
-// 	private MyDrive md;
+import pt.tecnico.mydrive.exceptions.InvalidPathException;
+import pt.tecnico.mydrive.exceptions.FileNotFoundException;
 
 
-// 	public void populate(){
-// 		md = MyDrive.getInstance();
-// 	}
+import pt.tecnico.mydrive.domain.MyDrive;
+import pt.tecnico.mydrive.domain.User;
+import pt.tecnico.mydrive.domain.File;
+import pt.tecnico.mydrive.domain.Directory;
+import pt.tecnico.mydrive.domain.Login;
 
-// 	@Test
-// 	public void success() {
-// 		new Expectations(){
-// 			{
-// 				md.getFileNameByUser(userName);
-// 				result = fileName;
-// 			}
-// 		};
-// 		ReadFileService service = new ReadFileService(randomtoken, fileName);
-// 		service.execute();
-// 		assertEquals(service.result(),fileName);
+@RunWith(JMockit.class)
+public class ReadFileTest extends AbstractServiceTest {
 
-// 	}
+	private static final String userName = "claudia";
+	private static final String fileName = "file.txt";
+	Random rand = new Random();
+    long randomtoken = rand.nextLong();
 
-//    //  public void invalidTokenTest() {
+	@Mocked
+	private MyDrive md;
 
 
+	public void populate(){
+		md = MyDrive.getInstance();
+	}
 
-//    //      final String userName = "claudia";
-//    //      md.createUser(userName);
-//    //      long token = md.loginUser(userName,userName);
+	@Test
+	public void success() {
+		new Expectations(){
+			{
+				md.getFileNameByUser(userName);
+				result = fileName;
+			}
+		};
+		ReadFileService service = new ReadFileService(randomtoken, fileName);
+		service.execute();
+		assertEquals(service.result(),fileName);
 
-//    //      md.createFile(token, "file.txt", "textfile", "do not read");
+	}
 
-//    //      Random rand = new Random();
-//    //      long randomtoken = rand.nextLong();
-
-
-//    //      ReadFileService service = new ReadFileService(randomtoken, "file.txt");
-
-//    //      //assertEquals(log.warn(), service.execute());
-
-
-//    // }
-
-//    //  @Test(expected = FileNotFoundException.class)
-//    //  public void fileDoesNotExist(){
-
-//    //  	String userName = "claudia";
-//    //      md.createUser(userName);
-//    //      long token = md.loginUser(userName,userName);
+   //  public void invalidTokenTest() {
 
 
-//    //      ReadFileService service = new ReadFileService(token, "HowToDoAProject.txt");
 
-//    //      service.execute();
-//    //  }
+   //      final String userName = "claudia";
+   //      md.createUser(userName);
+   //      long token = md.loginUser(userName,userName);
+
+   //      md.createFile(token, "file.txt", "textfile", "do not read");
+
+   //      Random rand = new Random();
+   //      long randomtoken = rand.nextLong();
 
 
-// }
+   //      ReadFileService service = new ReadFileService(randomtoken, "file.txt");
+
+   //      //assertEquals(log.warn(), service.execute());
+
+
+   // }
+
+   //  @Test(expected = FileNotFoundException.class)
+   //  public void fileDoesNotExist(){
+
+   //  	String userName = "claudia";
+   //      md.createUser(userName);
+   //      long token = md.loginUser(userName,userName);
+
+
+   //      ReadFileService service = new ReadFileService(token, "HowToDoAProject.txt");
+
+   //      service.execute();
+   //  }
+
+
+}
