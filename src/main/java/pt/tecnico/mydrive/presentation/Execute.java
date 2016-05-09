3a4a62d7dd4040d1a1d29455e55c6dad15c8ec68
MargaridaCorreia <margarida.correia@ist.utl.pt>
2016-05-09 17:48:53 +0100

    Commit by rafa32: execute presentation command done

--- src/main/java/pt/tecnico/mydrive/presentation/Execute.java
+++ src/main/java/pt/tecnico/mydrive/presentation/Execute.java
@@ -1,24 +1,53 @@
-// package pt.tecnico.mydrive.presentation;
+package pt.tecnico.mydrive.presentation;
 
-// import pt.tecnico.mydrive.service.ExecuteFileService;
+import pt.tecnico.mydrive.service.ExecuteFileService;
 
-// public class Execute extends MdCommand{
+public class Execute extends MdCommand{
 
-// 	public Execute(Shell sh){
-// 		super(sh, "do", "Execute the file identified by the given path with the given arguments");
+	public Execute(Shell sh){
+		super(sh, "do", "Execute the file identified by the given path with the given arguments");
 
-// 	}
+	}
 
-// 	public void execute(String[] args) { 
+	public void execute(String[] args) { 
 
-// 		if(args.length < 3){
+		if(args.length < 2){
 
-// 			throw new RuntimeException("USAGE: "+name()+ " <args>");
+			throw new RuntimeException("USAGE: "+name()+ " <path> [<args>]");
 
-// 		}
-// 			ExecuteFileService efs = new ExecuteFileService(args[1], args);
-// 			efs.execute();
+		}
 
-// 		}
+		if(args.length > 2){
 
-// 	}
+			String activeUser = this.shell().getActiveUser();
+			long token = this.shell().getTokenByUser(activeUser);
+			
+			
+
+
+			String[] serviceArgs = new String[args.length-1];
+
+			for(int i=1; i < serviceArgs.length - 1; i++){
+
+				serviceArgs[i] = args[i];
+
+
+			}
+
+			ExecuteFileService efs = new ExecuteFileService(token, args[0], serviceArgs );
+			efs.execute();
+
+		}
+		else{
+
+			String activeUser = this.shell().getActiveUser();
+			long token = this.shell().getTokenByUser(activeUser);
+			
+
+			ExecuteFileService efs = new ExecuteFileService(token, args[0]);
+			efs.execute();
+
+		}
+
+	}
+}
--- src/main/java/pt/tecnico/mydrive/service/ExecuteFileService.java
+++ src/main/java/pt/tecnico/mydrive/service/ExecuteFileService.java
@@ -1,27 +1,69 @@
-// package pt.tecnico.mydrive.service;
+package pt.tecnico.mydrive.service;
+import pt.tecnico.mydrive.exceptions.MyDriveException;
 
-// import pt.tecnico.mydrive.exceptions.MyDriveException;
-
-// public class ExecuteFileService extends MyDriveService{
+public class ExecuteFileService extends MyDriveService{
 	
-// 	private long _token;
-// 	private String _path;
-// 	private String[] _args;
+	private long _token;
+	private String _path;
+	private String[] _args;
 	
-// 	public ExecuteFileService(long token, String path, String[] args) {
-// 		_token = token;
-// 		_path = path;
-// 		_args = args;
-// 	}
+	public ExecuteFileService(long token, String path, String[] args) {
+		_token = token;
+		_path = path;
+		_args = args;
 
-// 	@Override
-// 	protected void dispatch() throws MyDriveException {
-// 		try{
-// 			getMydrive().executeFile(_token, _path, _args);
-// 		}catch(MyDriveException e){
-// 			throw e;
-// 		}
+	}
 
-// 	}
+	public ExecuteFileService(long token, String path) {
+		_token = token;
+		_path = path;
+		
+	
+	}
 
-// }
+	public void setToken(long tken){
+
+   		 _token = tken;
+
+  	}
+
+	public void setPath(String path){
+
+   		 _path = path;
+
+  	}
+
+	public void setArgs(String[] args){
+
+   		 _args = args;
+
+  	}
+
+  	public long getToken(){
+
+    	return _token;
+
+  	}
+
+   	public String getPath(){
+
+    	return _path;
+
+  	}
+
+     public String[] getArgs(){
+
+    	return _args;
+
+  	}
+
+	protected void dispatch() throws MyDriveException {
+		try{
+			getMydrive().executeFile(_token, _path, _args);
+		}catch(MyDriveException e){
+			throw e;
+		}
+
+	}
+
+}
