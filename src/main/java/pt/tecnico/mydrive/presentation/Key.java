5f6ca5b17ace182df6b3ab404b3f4dd34acdda8b
MargaridaCorreia <margarida.correia@ist.utl.pt>
2016-05-10 12:23:27 +0100

    Pequenas modificacoes em keyCommand

--- src/main/java/pt/tecnico/mydrive/presentation/Key.java
+++ src/main/java/pt/tecnico/mydrive/presentation/Key.java
@@ -5,7 +5,7 @@ public class Key extends MdCommand{
 
   public Key(Shell sh){
 
-    super(sh, "token", "Changes the login session. If the armuments are omitted it prints the current session token");
+    super(sh, "token", "Changes the login session. If the arguments are omitted it prints the current session token");
 
   }
 
@@ -17,17 +17,22 @@ public class Key extends MdCommand{
 
     }
 
+    //invocado com username
     if(args.length == 1) {
 
-      this.shell().setActiveUsers(args[0]);
-
+      String activeUser = this.shell().setActiveUsers(args[0]);
+      long token = this.shell().getTokenByUser(activeUser);
+      System.out.println(token);
+      
     }
 
+    //invocado sem argumentos
    	else {
 
       String activeUser = this.shell().getActiveUser();
 			long token = this.shell().getTokenByUser(activeUser);
       System.out.println(token);
+      System.out.println(username);
 
     }
 
