705009cf7cb807957d7e38d120dcdffa4be57be6
claudiaamorim <ana-claudia1995@hotmail.com>
2016-05-10 08:33:40 +0100

    Closes #30

--- src/main/java/pt/tecnico/mydrive/domain/Application.java
+++ src/main/java/pt/tecnico/mydrive/domain/Application.java
@@ -19,6 +19,7 @@ public class Application extends Application_Base {
         set_content(content);   
    } 
 
+
    @Override
    public void setAssociation(Association assoc) throws ApplicationDoesntHasAssocException{
     throw new ApplicationDoesntHasAssocException();
